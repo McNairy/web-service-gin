0fc4f08145743ecfc292f16ba33c4452ac82a157
Michael McNairy <michaelmcnairy@hotmail.com>
2024-03-13 21:22:15 -0700

    WIP adding swagger docs

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"net/http"
 
+	"github.com/McNairy/web-service-gin/docs"
 	"github.com/gin-gonic/gin"
+	docs "github.com/go-project-name/docs"
+	"github.com/swaggo/gin-swagger"
 )
 
 type album struct {
@@ -17,6 +20,32 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// @BasePath /
+
+// AlbumsExample godoc
+// @Summary albums example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 array album
+// @Router /albums [get]
+
+func main() {
+	router := gin.Default()
+
+	docs.SwaggerInfo.BasePath = "/docs"
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	router.Run(":8080")
+
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
+	router.POST("/albums", postAlbums)
+	router.Run("localhost:8080")
 }
 
 func getAlbums(c *gin.Context) {
@@ -46,12 +75,4 @@ func getAlbumById(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumById)
-	router.POST("/albums", postAlbums)
-	router.Run("localhost:8080")
 }
